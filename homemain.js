1f54e834a1b669850ab445f41c5cfab667eb94ec
Pwewak <kitzeru@gmail.com>
2023-11-10 14:15:17 +0530

    second commit

---
+++ homemain.js
@@ -0,0 +1,39 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.SphereGeometry( 6,32,16);
+const material = new THREE.MeshBasicMaterial( { color:"silver",wireframe:true } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+camera.position.z = 5;
+
+function onWindowResize() {
+			
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
+
+
+
+function animate() {
+	requestAnimationFrame( animate );
+
+
+	sphere.rotation.x += 0.001;
+	sphere.rotation.y += 0.001;
+
+
+	renderer.render( scene, camera );
+    onWindowResize();
+}
+
+animate();
---
+++ snakegamehome/snake.js
@@ -0,0 +1,98 @@
+var blockSize = 25;
+var rows = 25;
+var cols = 25;
+var board;
+var context;
+
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var vx = 0;
+var vy = 0;
+
+var snakeBody = [];
+
+var foodX;
+var foodY;
+
+var gameover = false;
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d");
+  placefood();
+  document.addEventListener("keyup", changeDirection);
+  // update();
+  setInterval(update, 1000 / 10);
+};
+
+function update() {
+  if (gameover) {
+    return;
+  }
+  context.fillStyle = "black";
+  context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = "Red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  if (snakeX == foodX && snakeY == foodY) {
+    snakeBody.push([foodX, foodY]);
+    placefood();
+  }
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
+  context.fillStyle = "lime";
+  snakeX += vx * blockSize;
+  snakeY += vy * blockSize;
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
+
+  if (
+    snakeX < 0 ||
+    snakeX > cols * blockSize ||
+    snakeY < 0 ||
+    snakeY > rows * blockSize
+  ) {
+    gameover = true;
+    alert("Game Over");
+  }
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+      gameover = true;
+      alert("Game Over");
+    }
+  }
+}
+
+function changeDirection(e) {
+  if (e.code == "KeyW" && vy != 1) {
+    vx = 0;
+    vy = -1;
+  }
+  if (e.code == "KeyS" && vy != -1) {
+    vx = 0;
+    vy = 1;
+  }
+  if (e.code == "KeyA" && vx != 1) {
+    vx = -1;
+    vy = 0;
+  }
+  if (e.code == "KeyD" && vx != -1) {
+    vx = 1;
+    vy = 0;
+  }
+}
+
+function placefood() {
+  foodX = Math.floor(Math.random() * cols) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
+}
